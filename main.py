1b0cd5dd2f5e296b3aa5d3c3364667f0d48de52f
thanmay <thanmaym0804@gmail.com>
2024-10-05 07:12:12 +0000

    updated with openings

--- main.py
+++ main.py
@@ -1,11 +1,42 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+PLACES = [
+{
+    'id':1,
+    'type':'Desert Photography',
+    'location': 'Rajasthan,India',
+    'salary': 'Rs.10,00,000'
+    
+},
+    {
+        'id':2,
+        'type':'Ocean Photography',
+        'location': 'Lakshadweep island,India',
+        'salary': 'Rs.1,00,000'
+
+    },
+{
+    'id':3,
+    'type':'Documentary Photography',
+    'location': 'Gujarat,India',
+    'salary': 'Rs.15,00,000'
+
+},
+{
+    'id':4,
+    'type':'Astro Photography',
+    'location': 'Tamilnadu,India',
+    'salary': 'Rs.24,00,000'
+
+}
+    
+]
 
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html',places=PLACES)
 
 
 if __name__ == '__main__':
